a1c44a9f1e3de1ea106b35d0fa893742ab5a8611
ts061282 <115440342+ts061282@users.noreply.github.com>
2024-12-05 19:03:21 -0600

    Day 4, Part 2 refactored and completed

--- Day4b.py
+++ Day4b.py
@@ -1,34 +1,13 @@
-def xmasAs(input = []):
-    asCoords = []
-    for line in range(0,len(input)):
-        for position in range(0,len(input[line])-2):
-            if input[line][position:position+3] == "MAS" or input[line][position:position+3] == "SAM":
-                asCoords.append((line,position+1))
-    return asCoords
-
-import re
 import importer
-input = importer.parseInput(1)
-
-tiltInput = ['' for i in range(0,len(input[0])+len(input)-1)]
-for line in range(0,len(input)):
-    for char in range(0,len(input[line])):
-        tIndex = line + char
-        tiltInput[tIndex] = input[line][char] + tiltInput[tIndex]
-
-transpTiltInput = ['' for i in range(0,len(input[0])+len(input)-1)]
-for line in range(0,len(input)):
-    for char in range(len(input[line])-1,-1,-1):
-        tIndex = line + (len(input[line])-1-char)
-        transpTiltInput[tIndex] = transpTiltInput[tIndex] + input[line][char]
+input = importer.parseInput(0)
 
 answer = 0
 
-tiltAs = xmasAs(tiltInput)
-transpTiltAs = xmasAs(transpTiltInput)
-
-for aye in tiltAs:
-    if (aye[1],aye[0]) in transpTiltAs: #need to transpose coordinate of As correctly to compare
-        answer += 1
+for line in range(1,len(input)-1):
+    for char in range (1,len(input[line])-1):
+        if input[line][char] == 'A':
+            if (input[line-1][char-1] == 'M' and input[line+1][char+1] == 'S') or (input[line-1][char-1] == 'S' and input[line+1][char+1] == 'M'):
+                if (input[line-1][char+1] == 'M' and input[line+1][char-1] == 'S') or (input[line-1][char+1] == 'S' and input[line+1][char-1] == 'M'):
+                    answer += 1
 
 print(answer)
