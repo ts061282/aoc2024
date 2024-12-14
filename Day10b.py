149b073471031693f412c070503e49466f14f622
ts061282 <115440342+ts061282@users.noreply.github.com>
2024-12-13 22:23:02 -0600

    Day 10 complete

---
+++ Day10b.py
@@ -0,0 +1,47 @@
+def coordsInList(coords = [0,0], coordList = [[0,0]]):
+    coords=coords[0]
+    for coord in coordList:
+        if coord[0] == coords[0] and coord[1] == coords[1]:
+            return True
+    return False
+
+def mapTrails(trails,input):
+    newTrails = []
+    for trail in trails:
+        stepCoords = trail[len(trail)-1]
+        stepVal = int(input[stepCoords[0]][stepCoords[1]])
+        if stepCoords[0]-1 >= 0:
+            if int(input[stepCoords[0]-1][stepCoords[1]]) == stepVal + 1:
+                newTrails += [trail + [[stepCoords[0]-1,stepCoords[1]]]]
+        if stepCoords[0]+1 < len(input):
+            if int(input[stepCoords[0]+1][stepCoords[1]]) == stepVal + 1:
+                newTrails += [trail + [[stepCoords[0]+1,stepCoords[1]]]]
+        if stepCoords[1]-1 >= 0:
+            if int(input[stepCoords[0]][stepCoords[1]-1]) == stepVal + 1:
+                newTrails += [trail + [[stepCoords[0],stepCoords[1]-1]]]
+        if stepCoords[1]+1 < len(input[stepCoords[0]]):
+            if int(input[stepCoords[0]][stepCoords[1]+1]) == stepVal + 1:
+                newTrails += [trail + [[stepCoords[0],stepCoords[1]+1]]]
+    if len(newTrails) == 0:
+        return None
+    if stepVal == 8:
+        return newTrails
+    else:
+        return mapTrails(newTrails, input)
+
+import importer
+input = importer.parseInput(1)
+
+trailheads = []
+for row in range(0,len(input)):
+    for col in range(0,len(input[row])):
+        if int(input[row][col]) == 0:
+            trailheads += [[row,col]]
+
+answer = 0
+for trailhead in trailheads:
+    trails = mapTrails([[trailhead]], input)
+    if not trails == None:
+        answer += len(trails)
+
+print(answer)
